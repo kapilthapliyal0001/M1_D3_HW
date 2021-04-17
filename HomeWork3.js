a6f4e33a58bb2b7ca3109cd4442e1d65147f6fb3
Kapil Thapliyal <kapilthapliyal0001@gmail.com>
2021-04-17 23:48:29 +0200

    Finished HW
    Completed homework

--- HomeWork3.js
+++ HomeWork3.js
@@ -29,6 +29,16 @@ console.log(new_arr);
 Write the code to get the maximum value in an array.
 */
 
+let max_ele = arr[0];
+
+for (i = 0; i < arr.length; i++) {
+    if (arr[i] > max_ele) {
+        max_ele = arr[i];
+    }
+}
+
+console.log(max_ele);
+
 /* WRITE YOUR CODE HERE */
 
 /* EXERCISE 3
@@ -36,6 +46,16 @@ Write the code to get the minimum value in an array.
 */
 
 /* WRITE YOUR CODE HERE */
+let min_ele = arr[0];
+
+for (i = 0; i < arr.length; i++) {
+    if (arr[i] < min_ele) {
+        min_ele = arr[i];
+    }
+}
+
+console.log(min_ele);
+
 
 /* EXERCISE 4
 Write the code to get only even numerical values in an array.
@@ -43,11 +63,29 @@ Write the code to get only even numerical values in an array.
 
 /* WRITE YOUR CODE HERE */
 
+arr = ["abc", 23, "erv", 32, 42, "ka4"];
+let num_arr = [];
+
+for (i = 0; i < arr.length; i++) {
+    if (isNaN(arr[i]) === false) {
+        num_arr.push(arr[i]);
+    }
+}
+
+console.log(num_arr);
+
 /* EXERCISE 5
 Write the code to delete even entries from an array.
 */
 
 /* WRITE YOUR CODE HERE */
+
+for(i = 0; i < arr.length; i++) {
+    if (i+1 % 2 === 0) {
+        arr.splice(i, 1);
+    }
+}
+console.log(arr);
 
 /* EXERCISE 6
 Write the code to remove all the vowels from a string.
@@ -55,11 +93,28 @@ Write the code to remove all the vowels from a string.
 
 /* WRITE YOUR CODE HERE */
 
+let string = "aeioujnsaocjencoewnoc";
+
+for(i of string) {
+    if (i in ['a', 'e', 'i', 'o', 'u']) {
+        string.replace(i, "");
+    }
+}
+
+console.log(string);
+
 /* EXERCISE 7
 Write the code to increase all the numeric values in a array by 1.
 */
 
 /* WRITE YOUR CODE HERE */
+let arr_new = [1, 57, 743, 7547, 47];
+let arr_inc = [];
+
+for (i = 0; i < arr_new.length; i++) {
+    arr_inc.push(arr_new[i] + 1);
+}
+console.log(arr_inc);
 
 /* EXERCISE 8 
 Replace all the strings in an array with their length.
@@ -67,6 +122,15 @@ es.: ["strive", "is", "great"] => [6, 2, 5]
 */
 
 /* WRITE YOUR CODE HERE */
+
+let str_len = ["strive", "is", "great"];
+let str_len_res = [];
+
+for (i of str_len) {
+    str_len_res.push(i.length);
+}
+
+console.log(str_len_res);
 
 /* WHEN YOU ARE FINISHED
 Send the code on the Eduflow platform. In the next days we'll also learn how to use GIT
